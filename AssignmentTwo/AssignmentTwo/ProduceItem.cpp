25593cc6d0db36e6b0dff212764565258df962ce
Megan Chipman <m.chipman52@gmail.com>
2018-10-17 14:43:39 -0400

    Added ProduceItem Class
    Class that creates nodes of produce items for a linked list.

---
+++ AssignmentTwo/AssignmentTwo/ProduceItem.cpp
@@ -0,0 +1,24 @@
+/*
+Megan Chipman
+Computer Science I
+Assignment Two
+October 17, 2018
+*/
+
+#include "stdafx.h"
+#include "ProduceItem.h"
+
+//Produce Item Constructor: 
+ProduceItem::ProduceItem(std::string produce, std::string type, std::string soldBy, float price, int quantityInStock)
+{
+	this->produce = produce; 
+	this->type = type; 
+	this->soldBy = soldBy; 
+	this->price = price; 
+	this->quantityInStock = quantityInStock; 
+}
+
+
+ProduceItem::~ProduceItem()
+{
+}
---
+++ AssignmentTwo/AssignmentTwo/ProduceItem.h
@@ -0,0 +1,25 @@
+/*
+Megan Chipman
+Computer Science I
+Assignment Two
+October 17, 2018
+*/
+
+#pragma once
+
+class ProduceItem
+{
+	public:
+		//Functions: 
+		ProduceItem(std::string produce, std::string type, std::string soldBy, float price, int quantityInStock);
+		~ProduceItem();
+
+		//Variables: 
+		std::string produce;
+		std::string type; 
+		std::string soldBy; 
+		float price; 
+		int quantityInStock; 
+		ProduceItem* next; 
+};
+
